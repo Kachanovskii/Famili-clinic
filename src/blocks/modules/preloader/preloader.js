a95e6426ac09aa218de7a3620eb590f71d90b854
Parchuk <sergeyparchuk1@gmail.com>
2020-08-20 14:06:01 +0300

    created footer

--- src/blocks/modules/preloader/preloader.js
+++ src/blocks/modules/preloader/preloader.js
@@ -1,10 +1,8 @@
-// document.body.onload = function () {
-
+// document.body = addEventListener('load', function () {
 //     setTimeout(function () {
 //         let preloader = document.querySelector('#page-preloader');
 //         if (!preloader.classList.contains('done')) {
 //             preloader.classList.add('done');
 //         }
 //     }, 1000);
-
-// }
+// });
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,3 +1,3 @@
+import "%modules%/preloader/preloader";
 import "%modules%/header/header";
 import "%modules%/footer/footer";
-import "%modules%/preloader/preloader";

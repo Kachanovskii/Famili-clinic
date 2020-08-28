e603f335d110b407f4be9aadf3ffe2e4e4bd500f
Kachanovskii <vadimskyi1@gmail.com>
2020-08-28 20:59:24 +0300

    slider

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,4 +1,5 @@
 import "%modules%/header/header";
+import "%modules%/banner/banner";
 import "%modules%/appointment/appointment";
 import "%modules%/footer/footer";
 import "%modules%/preloader/preloader";

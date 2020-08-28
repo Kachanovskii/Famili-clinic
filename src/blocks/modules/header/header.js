3549bfb353447bf311ce744745c33c7a943b7770
Kachanovskii <vadimskyi1@gmail.com>
2020-08-28 14:08:30 +0300

    changув the header

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -3,20 +3,16 @@ window.onscroll = function () { headerFixed(); };
 let header = document.querySelector('.header');
 let sticky = header.offsetTop;
 function headerFixed() {
-    console.log(header.offsetTop);
     if (window.pageYOffset > sticky) {
-
         header.classList.add('header-fixed');
-    }
-    else if (window.pageYOffset == 0) {
-        header.classList.remove('header-fixed');
     }
     else {
-        header.classList.add('header-fixed');
+        header.classList.remove('header-fixed');
     }
 };
 // mobile-btn
 document.querySelector('.mobile').addEventListener('click', function () {
+    document.querySelector('.header').classList.toggle('header-mobile-menu');
     document.querySelector('.bar1').classList.toggle('bar1-active');
     document.querySelector('.bar2').classList.toggle('bar2-active');
     document.querySelector('.bar3').classList.toggle('bar3-active');

13e5b6007583d3955c3311ece13703b562e7d93d
Parchuk <sergeyparchuk1@gmail.com>
2020-08-30 18:08:30 +0300

    add plagin owlCarousel

--- src/blocks/modules/preloader/preloader.js
+++ src/blocks/modules/preloader/preloader.js
@@ -1,9 +1,9 @@
-body = addEventListener('load', function () {
-    setTimeout(function () {
-        let preloader = document.querySelector('#page-preloader');
-        if (!preloader.classList.contains('done')) {
-            preloader.classList.add('done');
-        }
-    }, 1000);
-});
+// body = addEventListener('load', function () {
+//     setTimeout(function () {
+//         let preloader = document.querySelector('#page-preloader');
+//         if (!preloader.classList.contains('done')) {
+//             preloader.classList.add('done');
+//         }
+//     }, 1000);
+// });
 

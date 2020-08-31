e6539600cbc3b6f5f6e92059ba9d58be8c59c95a
Parchuk <sergeyparchuk1@gmail.com>
2020-08-31 17:32:56 +0300

    finished block ourDoctors and add library owl.carousel.js

--- src/blocks/modules/ourDoctors/ourDoctors.js
+++ src/blocks/modules/ourDoctors/ourDoctors.js
@@ -1,4 +1,27 @@
 
+// *******************   ourDoctors  **************************
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel();
+    $('.owl-two').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        dots: false,
+        smartSpeed: 900,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            575.98: {
+                items: 3,
+                nav: true
+            },
+            991.98: {
+                items: 4,
+                nav: true,
+                loop: false
+            }
+        }
+    });
 });
+// *******************   ourDoctors  **************************

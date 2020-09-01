782ec9c9e24e38d899d957246dae6bcecbe5a13d
Parchuk <sergeyparchuk1@gmail.com>
2020-09-01 15:14:23 +0300

    created block ourAdvantages

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,7 +1,7 @@
 import "%modules%/header/header";
-import "%modules%/banner/banner";
 import "%modules%/reviewsOfDoctors/reviewsOfDoctors";
 import "%modules%/appointment/appointment";
 import "%modules%/footer/footer";
 import "%modules%/preloader/preloader";
+
 

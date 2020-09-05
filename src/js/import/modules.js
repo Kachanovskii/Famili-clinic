131e1f5000d0c26c70c2fd4172d1198e677d864c
Parchuk <sergeyparchuk1@gmail.com>
2020-09-05 21:49:01 +0300

    added change in style some blocks

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -4,5 +4,7 @@ import "%modules%/appointment/appointment";
 import "%modules%/footer/footer";
 import "%modules%/preloader/preloader";
 import "%modules%/popupWindowAppointment/popupWindowAppointment";
+import "%modules%/callbackButton/callbackButton";
+
 
 

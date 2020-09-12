8673179b418d975deb70ddeb802b04e961244f64
Parchuk <sergeyparchuk1@gmail.com>
2020-09-12 12:29:35 +0300

    worked with paga Doctors And Referrals and fixed position

--- src/blocks/modules/upButton/upButton.js
+++ src/blocks/modules/upButton/upButton.js
@@ -3,12 +3,12 @@ jQuery(document).ready(function () {
     window.addEventListener('scroll', function () {
         if ($(window).scrollTop() > 600) {
             btn.addClass('show');
-            if (($(window).scrollTop() + $(window).height()) > ($(document).height() - ($(".footer_main").height() + $(".appointment").height()))) {
-                $(".footer_main").css("display", "block");
+            if (($(window).scrollTop() + $(window).height()) > ($(document).height() - ($(".footer_main").height() + 280))) {
+                $(".footer_main").css("opacity", "1");
                 $("body").css("background-color", "#0c4459");
             } else {
                 $("body").css("background-color", "#fcfcfc");
-                $(".footer_main").css("display", "none");
+                $(".footer_main").css("opacity", "0");
             }
         }
         else {
@@ -17,7 +17,7 @@ jQuery(document).ready(function () {
         }
     });
     btn.on('click', function (e) {
-        $(".footer_main").css("display", "none");
+        $(".footer_main").css("opacity", "0");
         $("body").css("background-color", "#fcfcfc");
         $('html, body').animate({ scrollTop: 0 }, 700);
     });

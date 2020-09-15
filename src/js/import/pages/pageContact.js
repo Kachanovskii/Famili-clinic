5386e0542c7c771da9739a23481ae1d4606f14a2
Parchuk <sergeyparchuk1@gmail.com>
2020-09-15 13:33:56 +0300

    added content in page Contact

---
+++ src/js/import/pages/pageContact.js
@@ -0,0 +1,8 @@
+import "%modules%/upButton/upButton";
+import "%modules%/header/header";
+import "%modules%/appointment/appointment";
+import "%modules%/footer/footer";
+import "%modules%/preloader/preloader";
+import "%modules%/popupWindowAppointment/popupWindowAppointment";
+import "%modules%/callbackButton/callbackButton";
+import "%modules%/mainContact/mainContact";

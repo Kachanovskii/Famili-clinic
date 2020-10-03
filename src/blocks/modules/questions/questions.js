a6a8ef9697d24226bd85b5d6016e3f4ebf315965
Kachanovskii <vadimskyi1@gmail.com>
2020-10-03 22:32:53 +0300

    changed the style of the section with questions size, color

--- src/blocks/modules/questions/questions.js
+++ src/blocks/modules/questions/questions.js
@@ -1,26 +1,20 @@
-
-console.log('work');;
 let toggle = false;
-let list = document.querySelectorAll('.prices-list__item');
+let list = document.querySelectorAll('.questions-sections__item');
 for (let e of list) {
   e.addEventListener('click', function () {
-    console.log(e.children);
     if (toggle == false) {
+      console.log(e.children);
+      e.children[0].classList.add('list-i-active');
       e.children[1].style.paddingBottom = '10px';
-      e.style.color = '#04aac7';
       e.children[2].style.transform = 'rotate(90deg)';
-      console.log(e.children[2].children[0]);
-      // e.children[2].style.display = 'table';
-      e.children[3].classList.add('show__table');
-
+      e.children[3].classList.add('show__sections');
       toggle = true;
     }
     else {
       e.children[1].style.paddingBottom = '0px';
-      e.style.color = '#505050';
+      e.children[0].classList.remove('list-i-active');
       e.children[2].style.transform = 'rotate(0deg)';
-      e.children[3].classList.remove('show__table');
-      // e.children[2].style.display = 'none';
+      e.children[3].classList.remove('show__sections');
       toggle = false;
     }
   });

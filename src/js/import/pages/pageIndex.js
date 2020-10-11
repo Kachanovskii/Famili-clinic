6ebb6c0cc84a49a6b9a41ccad00f290f78b4c121
Parchuk <sergeyparchuk1@gmail.com>
2020-10-11 19:40:00 +0300

    added library wow

--- src/js/import/pages/pageIndex.js
+++ src/js/import/pages/pageIndex.js
@@ -2,4 +2,12 @@ import "../carousel/owl.carousel";
 import "%modules%/banner/banner";
 import "%modules%/ourDoctors/ourDoctors";
 import "%modules%/upButton/upButton";
+const WOW = require('../wow/wow.min.js');
 
+
+window.wow = new WOW.WOW({       // default
+    mobile: true,       // default
+    live: false       // default
+});
+
+window.wow.init();

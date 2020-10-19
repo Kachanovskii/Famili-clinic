439ed7c850872e4ba4a238c6025184ce7620f44b
Parchuk <sergeyparchuk1@gmail.com>
2020-10-19 13:36:07 +0300

    added sidebar

--- src/blocks/modules/mainDoctorsAndReferrals/mainDoctorsAndReferrals.js
+++ src/blocks/modules/mainDoctorsAndReferrals/mainDoctorsAndReferrals.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-    $('#navbar-icon').click(function () {
+    $('.navbar-icon-wrapper').click(function () {
         $(this).toggleClass('open');
         $('.mainDoctorsAndReferrals__navbar').toggleClass('open');
-        // $('body').toggleClass('scroll-hidden');
+        $('body').toggleClass('scroll-hidden');
 
     });
     $('.mainDoctorsAndReferrals__navbar').click(function (e) {
         if ($(e.target).closest('.mainDoctorsAndReferrals__navbar-content').length == 0) {
-            $('.mainDoctorsAndReferrals__navbar, #navbar-icon').removeClass('open');
-            // $('body').removeClass('scroll-hidden');
+            $('.mainDoctorsAndReferrals__navbar, .navbar-icon-wrapper').removeClass('open');
+            $('body').removeClass('scroll-hidden');
         }
     });
 });

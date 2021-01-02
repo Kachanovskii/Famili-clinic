e6588d56694398893289d322bed7b79b4c1b0aa9
Kachanovskii <vadimskyi1@gmail.com>
2021-01-02 11:47:49 +0200

    added fetch function from form a footer

--- src/blocks/modules/appointment/appointment.js
+++ src/blocks/modules/appointment/appointment.js
@@ -24,11 +24,49 @@ const select = new Select('#select', {
 });
 //  *******  end castom select  ********
 
-document.querySelector('.contact-form__button input').addEventListener('click', (e) => {
+document.querySelector('.contact-form__button button').addEventListener('click', (e) => {
     e.preventDefault();
-});;
+});
 
+const moment = require('moment');
+document.getElementById('appointment__form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    let $service = select.current.value;
+    let $phone = document.querySelector('#appointment__form input[name="phone"]').value;
+    let $name = document.querySelector('#appointment__form input[name="name"]').value;
+    let $date = document.querySelector('#appointment__form input[name="date"]').value;
 
+    let newAppointment = {
+        service: $service,
+        name: $name,
+        phone: $phone,
+        date: $date
+    };
+
+    let dateNow = moment(new Date()).format('MMMM Do YYYY, h:mm:ss a');
+    fetch(`https://api.telegram.org/bot1461272815:AAELZnj3YdSHVItWSyK0N8wHkQkPIdRl-eQ/sendMessage?chat_id=-311294005&text=New+Appointment+from+Family+Clinic%0AService:+${newAppointment.service}%0AName:+${newAppointment.name}%0APhone:+${'%2B' + newAppointment.phone}%0AAppointment+date:+${newAppointment.date}%0AApplication time:+${dateNow}`).then(responce => {
+        return responce.json();
+    }).then(data => {
+        let $statusMessage = document.querySelector('.appointment_message');
+        let status = '';
+
+        if (data.ok) {
+            status = 'success';
+            $statusMessage.classList.add(status);
+            $statusMessage.innerHTML = 'Ваша заявка успішно подана 🙂 !';
+        } else {
+            status = 'error';
+            $statusMessage.classList.add(status);
+            $statusMessage.innerHTML = 'Виникла помилка 🙁 спробуйте ще раз!';
+        }
+        setTimeout(() => {
+            $statusMessage.classList.remove(status);
+        }, 5000);
+        console.log(data);
+    });
+
+    console.log(newAppointment);
+});
 
 //  *******  start appointment fetch  ********
 // const URL = 'http://localhost';
